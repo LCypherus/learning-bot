2e9e350b0bfdaa866b70849d0b06289ec2514517
LCypherus <km1609@gmail.com>
2021-05-19 11:06:10 +0200

    Finished video 6

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # learning-bot
 This is a tutorial bot by following the discord js tutorial of Worn Off Keys on Youtube.
 
-## Followed video: 5
+## Followed video: 6 (Basic command handler)
 
 ## Bot functions:
+- Basic Command Handler
+- &ping --> "Pong!"
--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
-// const config = require('./config.json')
+const command = require('./command.js')
 
 client.on('ready', () => {
     console.log('The client is ready!')
+
+    command(client, 'ping', (message) => {
+        message.channel.send('Pong!')
+    })
 })
 
+require('dotenv').config();
 client.login(process.env.BOTTOKEN);
